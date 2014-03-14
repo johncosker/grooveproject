4b3297bb4398e49a0707d62fab45e39667331f09
johncosker <cosker.john@gmail.com>
2014-03-14 01:06:20 +0000

    main.py now has vlc python bindings example

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
 import utils
+import vlc
+import time
+from grooveshark import Client
+client = Client()
+client.init()
+songs = client.search("Wake me up", type='Songs')
+song = songs.next()
+vlc_args = ["-q"]
+instance = vlc.libvlc_new(1,vlc_args)
 
-utils.play("Get Lucky")
+player = instance.media_player_new()
+player.set_mrl(song.stream.url)
+player.play()
+time.sleep(500)
