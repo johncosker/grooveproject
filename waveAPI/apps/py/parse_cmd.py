5f33b60f6a9b00b4297cc38eaf55d5c27cfe7631
chrieff <chrieff@outlook.com>
2014-11-17 17:10:14 -0500

    stuff

--- waveAPI/apps/py/parse_cmd.py
+++ waveAPI/apps/py/parse_cmd.py
@@ -4,44 +4,6 @@ import logging
 from song_control import songs_controller
 from groove_control import groove_controller
 
-def sqliteManager(data):
-    song_controller = songs_controller(1)
-    if data['cmd'] == 'addSong':
-        song_controller.addKnownSong({'song':   data['song'],
-                                      'artist': data['artist'],
-                                      'stream': data['stream']})
-    elif data['cmd'] == 'addSongAndroid':
-        arr = data['info'].split(';')
-        song_controller.addKnownSong({'song':   arr[0],
-                                      'artist': arr[1],
-                                      'stream': arr[2]})
-    elif data['cmd'] == 'addSongBySourceType':
-        song_controller.addSongBySourceType(data)
-    elif data['cmd'] == 'fetchdb':
-        pass
-
-
-class SearchHandler():
-    def search(self, string):
-        gc = groove_controller(None)
-        return gc.getManySongs(string, 20)
-
-    def handle(self, string):
-        d = threads.deferToThread(self.search, string)
-        d.addCallback(self.ret)
-        d.addErrback(self.err)
-
-    def ret(self, data):
-        response = {}
-        response['type'] = 'json'
-        response['songs'] = data
-        response['queryType'] = 'search'
-        return response
-
-    def err(self, failure):
-        self.write('An error occured : %s\n' % failure.getErrorMessage())
-        print('An error occured : %s\n' % failure.getErrorMessage())
-
 
 class main_parser():
     """This takes all networks messages and parses the request commands and
@@ -51,38 +13,81 @@ class main_parser():
     def __init__(self, player_q):
         self.response = {}
         self.player_q = player_q
-        self._target_parse{'player': self._player,
-                           'dataBase': self._dataBase,
-                           'search': self._search}
-   
-    def self.parse_message(self, received_msg):
-        self.received_msg = received_msg
-        print('dataReceived')
-        logging.info(self.received_msg)
-        self._target_parse[self.received_msg['target']]()
-        return self.response
 
-    def self._player(self):
-        self.player_q.put(self.received_msg)
-        self.response['type'] = 'Message'
-        self.response['Message'] = 'Received'
+    def _player(self):
+        def player_player(self):
+                self.player_q.put(self.received_msg)
+                self.response['type'] = 'Message'
+                self.response['Message'] = 'Received'
 
-    def self._dataBase(self):
-        if self.received_msg['cmd'] == 'delete':
-            song_controller.removeSongById(self.received_msg['info'])
+        player_options = {'player' : player_player}
+        cmd = self.received_msg['cmd']
+        player_options[cmd](self)
+
+    def _dataBase(self):
+        def db_delete(self):
+            self.song_controller.removeSongById(self.received_msg['info'])
             self.response['type'] = 'Message'
             self.response['Message'] = 'Received'
 
-        elif self.received_msg['cmd'] == 'showdb':
+        def db_showdb(self):
             self.response['type'] = "json"
             self.response['queryType'] = 'database'
-            songs = song_controller.toArray()
+            songs = self.song_controller.toArray()
             self.response['songs'] = songs
 
-        sqliteManager(self.received_msg)
-        self.response['type'] = 'Message'
-        self.response['Message'] = 'Received'
+        def db_addSong(self):
+            self.song_controller.addKnownSong({'song': data['song'],
+                                               'artist': data['artist'],
+                                               'stream': data['stream']})
+            self.response['type'] = 'Message'
+            self.response['Message'] = 'Received'
 
-    def self._search(self):
-        h = SearchHandler()
-        self.response = h.handle(self.received_msg['cmd'])
+        def db_addSongAndriod(self):
+            self.song_controller.addKnownSong({'song': arr[0],
+                                               'artist': arr[1],
+                                               'stream': arr[2]})
+            self.response['type'] = 'Message'
+            self.response['Message'] = 'Received'
+
+        def db_addSongBySourceType(self):
+            self.song_controller.addSongBySourceType(data)
+            self.response['type'] = 'Message'
+            self.response['Message'] = 'Received'
+
+        def db_fetchdb(self):
+            self.response['type'] = 'Message'
+            self.response['Message'] = 'Received'
+
+        self.song_controller = songs_controller(1)
+        dataBase_options = {'delete': db_delete,
+                            'showdb': db_showdb,
+                            'addSong': db_addSong,
+                            'addSongAndroid': db_addSongAndriod,
+                            'addSongBySourceType': db_addSongBySourceType,
+                            'fetchdb': db_fetchdb}
+        target = self.received_msg['target']
+        dataBase_options[target](self)
+
+    def _search(self):
+        def search_search(self):
+                gc = groove_controller(None)
+                self.response['type'] = 'json'
+                self.response['songs'] = gc.getManySongs(string, 20)
+                self.response['queryType'] = 'search'
+
+        search_options = {'search': search_search}
+        target = self.received_msg['target']
+        search_options[target](self)
+
+    def parse_message(self, received_msg):
+        """Main parser function"""
+        target_parse = {'player': self._player,
+                        'dataBase': self._dataBase,
+                        'search':  self._search}
+        self.received_msg = received_msg
+        print('dataReceived')
+        logging.info(self.received_msg)
+        target = self.received_msg['target']
+        target_parse[target]()
+        return self.response
